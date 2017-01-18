dff5beddb799787b25605ab561d44b0721cb9aa3
Hiroki Kumamoto <kumabook@live.jp>
2017-01-18 11:15:32 +0900

    Remove nisemono.mock() because prefer to use nisemono.obj()

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var func        = require('./src/func');
 var obj         = require('./src/obj');
-var mock        = require('./src/mock');
 var Expectation = require('./src/expectation');
 var expects     = require('./src/expects');
 var promise     = require('./src/promise');
@@ -8,7 +7,6 @@ var promise     = require('./src/promise');
 module.exports = {
   func:        func,
   obj:         obj,
-  mock:        mock,
   expects:     expects,
   Expectation: Expectation,
   promise:     promise
